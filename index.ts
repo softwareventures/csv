9415898145213dc911f1c27a64b1b928b30115ca
Daniel Cassidy <mail@danielcassidy.me.uk>
2017-06-07 16:45:12 +0100

    Make CSV separator and quote character configurable.

--- index.ts
+++ index.ts
@@ -51,6 +51,14 @@ function endData(parseData: ParseData): ParseData {
     }
 }
 
+export interface Configuration {
+    readonly separator?: string;
+    readonly quote?: string;
+}
+
+const defaultSeparator = ",";
+const defaultQuote = '"';
+
 const initial = Object.freeze<ParseData>({
     state: "None",
     result: [],
@@ -58,28 +66,31 @@ const initial = Object.freeze<ParseData>({
     field: ""
 });
 
-export function parse(data: string): ReadonlyArray<ReadonlyArray<string>> {
+export function parse(data: string, configuration?: Configuration): ReadonlyArray<ReadonlyArray<string>> {
+    let separator = configuration && configuration.separator || defaultSeparator;
+    let quote = configuration && configuration.quote || defaultQuote;
+
     let resultState = data.split('')
             .reduce((data: ParseData, char: string) => {
                 if (data.state === "AfterComma" && char === " ") {
                     return data;
                 } else if (data.state === "InLineBreak" && char === "\n") {
                     return changeState(data, "None");
-                } else if (data.state === "AfterQuote" && char === '"') {
-                    return changeState(appendText(data, '"'), "InQuote");
+                } else if (data.state === "AfterQuote" && char === quote) {
+                    return changeState(appendText(data, quote), "InQuote");
                 } else if (data.state === "InQuote") {
-                    if (char === '"') {
+                    if (char === quote) {
                         return changeState(data, "AfterQuote");
                     } else {
                         return appendText(data, char);
                     }
-                } else if (char === ",") {
+                } else if (char === separator) {
                     return changeState(endField(data), "AfterComma");
                 } else if (char === "\r") {
                     return changeState(endRecord(data), "InLineBreak");
                 } else if (char === "\n") {
                     return changeState(endRecord(data), "None");
-                } else if (char === '"') {
+                } else if (char === quote) {
                     return changeState(data, "InQuote");
                 } else {
                     return changeState(appendText(data, char), "None");
@@ -89,10 +100,19 @@ export function parse(data: string): ReadonlyArray<ReadonlyArray<string>> {
     return endData(resultState).result;
 }
 
-export function write(table: string[][]): string {
+function regexEscape(text: string): string {
+    return text.replace(/[\\\^$*+?.()|{}\[\]]/g, c => "\\"+ c);
+}
+
+export function write(table: string[][], configuration?: Configuration): string {
+    let separator = configuration && configuration.separator || defaultSeparator;
+    let quote = configuration && configuration.quote || defaultQuote;
+
+    let quoteRegex = new RegExp(regexEscape(quote), "g");
+
     return table
             .map(row => row
-                    .map(field => '"' + field.replace(/"/g, '""') + '"')
-                    .join(","))
+                    .map(field => quote + field.replace(quoteRegex, quote + quote) + quote)
+                    .join(separator))
             .join("\r\n");
 }
